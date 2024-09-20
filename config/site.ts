f19a69f86ed612212a6d8aefcef06dc84b10e74b
Sahil Shenoy <sahilshenoy3@gmail.com>
2024-09-20 19:03:40 +0530

    added navbar and dock

---
+++ config/site.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+    name: "Sahil & Dhwani",
+    url: "https://sah-ani.com",
+    description: "Blog and Newsletter for our technology, thoughts and ideas, using NextJS, shadcm, and AWS SES",
+    author1: "Sahil Shenoy",
+    autho2: "Dhwani Budhiraja",
+    links: {
+      github1: "https://github.com/sahilshenoy",
+      github2: "https://github.com/dhwanibudhiraja",
+      personalSite1: "https://sahilshenoy.com",
+      personalSite2: "https://dhwanibudhiraja.com",
+    },
+  };
+  
+  export type SiteConfig = typeof siteConfig;
